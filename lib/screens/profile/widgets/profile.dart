affbb5ba5ba2c64a753317de5f063b78a54bf2e3
Andre Dippenaar <andre.pierre.dippenaar@gmail.com>
2022-02-07 10:22:12 +0200

    Custom AppBar

---
+++ lib/screens/profile/widgets/profile.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: _buildAppBar(),
+      body: const Center(
+        child: Text('Profile Page'),
+      ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+      leading: _buildIcon(
+        Icons.arrow_back_ios_outlined,
+      ),
+      actions: [
+        _buildIcon(
+          Icons.more_vert_outlined,
+        )
+      ],
+    );
+  }
+
+  IconButton _buildIcon(IconData icon) {
+    return IconButton(
+      onPressed: () {},
+      splashRadius: 25,
+      icon: Icon(
+        icon,
+        color: Colors.black,
+        size: 20,
+      ),
+    );
+  }
+}
